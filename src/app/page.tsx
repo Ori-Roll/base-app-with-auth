a9059210065bef4e2ceac98c895263372d75eacb
Ori Roll <oriroll@gmail.com>
2024-03-18 19:16:10 +0200

    login button name change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { LoginButton } from '@/components/auth/login-button';
+import { LoginButton } from '@/components/auth/LoginButton';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 
