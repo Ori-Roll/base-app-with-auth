8eca45411de4f2bbceb685e7f6b5ae47b4c673e5
Ori Roll <oriroll@gmail.com>
2024-03-20 15:13:23 +0200

    Added user authentication to register - using email to find existing - creating in db - hashing password using bcrypt

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,7 +1,10 @@
 'use server';
 
 import * as z from 'zod';
+import bcrypt from 'bcrypt';
+import { db } from '@/lib/db';
 import { RegisterSchema } from '@/schemas';
+import { getUserByEmail } from '@/data/user';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
@@ -13,5 +16,25 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: 'Invalid email or password' };
   }
 
-  return { success: 'validated' };
+  const { name, email, password } = validatedFields.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: 'User already exists' };
+  }
+
+  const newUser = {
+    name,
+    email,
+    password: hashedPassword,
+  };
+
+  await db.user.create({
+    data: newUser,
+  });
+
+  return { success: 'User created' };
 };
