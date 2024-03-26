f5489a6a97165ac2e4d12236d3ebdd191fd6d79f
Ori Roll <oriroll@gmail.com>
2024-03-26 12:43:07 +0200

    log cleanups

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,40 +14,24 @@ export default auth((req) => {
   const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
 
-  console.log('-------------------------- ');
-  console.log('MID url is ', nextUrl.pathname);
-  console.log('MID req.auth ', req.auth);
-
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-  console.log('MID - isApiAuthRoute ', isApiAuthRoute);
-
-  console.log('MID - isPublicRoute ', isPublicRoute);
-
-  console.log('MID - isAuthRoute ', isAuthRoute);
-
   if (isApiAuthRoute) {
-    console.log('MID - we are in api auth route');
     return;
   }
 
   if (isAuthRoute) {
-    console.log('MID - we are in auth route');
     if (isLoggedIn) {
-      console.log('MID - we are logged in');
       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
     }
-    console.log('MID - we are not logged in');
     return;
   }
 
   if (!isLoggedIn && !isPublicRoute) {
-    console.log('MID - we are not logged in and not in public route');
     return Response.redirect(new URL('/auth/login', nextUrl));
   }
-  console.log('MID - out');
   return;
 });
 
